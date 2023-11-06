a15cfb677b77e7a0f3408b886e4cd85d67c0b20b
Ivan Sorokin <i.sorok1n@icloud.com>
2023-11-06 12:56:06 +0000

    cleanup and readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Documents Mining Tool for Investigative Journalists
+
+The project **Sherlock & Claude** was implemented during [Anthropic London Hackathon 2023](https://cerebralvalley.notion.site/Anthropic-London-Event-Details-Hackers-54a4834bb79e40b8aa5b9bb8cd84347f).
+
+Implementation based on the following frameworks and APIs:
+* Anthropic Claude API
+* Google OCR API
+* Haystack (by deepset)
+* Brave Search API
+* Cohere Rerank API
+* Streamlit
+
+More details on Devpost [page](https://devpost.com/software/sherlock-claude).
--- ocr.py
+++ ocr.py
@@ -1,10 +1,8 @@
 import PyPDF2
 import numpy as np
-import pandas as pd
 from io import BytesIO
 from google.cloud import vision
-from collections import Counter, defaultdict
-from sklearn.cluster import DBSCAN, AgglomerativeClustering
+from sklearn.cluster import AgglomerativeClustering
 
 
 def get_text(w):
@@ -22,122 +20,6 @@ def get_text(w):
         elif s.property.detected_break.type == vision.TextAnnotation.DetectedBreak.BreakType.LINE_BREAK:
             text += " "
     return text
-
-
-def merge_2d(blocks, eps=0.05, min_samples=30, min_blocks=3, verbose=False):
-
-    # Group similar paragraphs with DBSCAN algorithm
-
-    points = []
-    for block in blocks:
-        for paragraph in block.paragraphs:
-            for word in paragraph.words:
-                for v in word.bounding_box.normalized_vertices:
-                    points.append((v.x, v.y))
-                v1, v2, v3, v4 = word.bounding_box.normalized_vertices
-                points.append(((v1.x + v2.x) / 2, (v1.y + v2.y) / 2))
-                points.append(((v2.x + v3.x) / 2, (v2.y + v3.y) / 2))
-                points.append(((v3.x + v4.x) / 2, (v3.y + v4.y) / 2))
-                points.append(((v4.x + v1.x) / 2, (v4.y + v1.y) / 2))
-                points.append(((v1.x + v3.x) / 2, (v1.y + v3.y) / 2))
-                points.append(((v2.x + v4.x) / 2, (v2.y + v4.y) / 2))
-
-    labels = DBSCAN(eps=eps, min_samples=min_samples).fit_predict(points)
-
-    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-    n_noise_ = list(labels).count(-1)
-
-    if verbose:
-        print("Estimated number of clusters: %d" % n_clusters_)
-        print("Estimated number of noise points: %d" % n_noise_)
-
-    n = 0
-    data = []
-    for i, block in enumerate(blocks):
-        for j, paragraph in enumerate(block.paragraphs):
-            cluster_ids = [labels[n] for _ in range(10) for _ in paragraph.words]
-            best_cluster_id = Counter(cluster_ids).most_common(1)[0][0]
-            n += len(cluster_ids)
-            if best_cluster_id != -1:
-                data.append([i, j, best_cluster_id])
-    df = pd.DataFrame(data, columns=["block", "paragraph", "cluster"])
-
-    new_paragraphs = []
-    merged_paragraphs = []
-    unmerged_paragraphs = []
-
-    for cluster_id, group in df.groupby("cluster"):
-        cluster = {block_id: rows.paragraph for block_id, rows in group.groupby("block")}
-        if len(cluster) < min_blocks:
-            continue
-        if verbose:
-            print(cluster_id)
-        new_paragraph = vision.Paragraph()
-        for block_id, paragraph_ids in cluster.items():
-            block = blocks[block_id]
-            for paragraph_id in paragraph_ids:
-                paragraph = block.paragraphs[paragraph_id]
-                for word in paragraph.words:
-                    # NOTE: hopefully paragraphs are sorted by y-axis
-                    new_paragraph.words.append(word)
-                merged_paragraphs.append(paragraph)
-            if verbose:
-                block_texts = []
-                for paragraph_id in paragraph_ids:
-                    paragraph_text = ""
-                    paragraph = block.paragraphs[paragraph_id]
-                    for word in paragraph.words:
-                        paragraph_text += get_text(word)
-                    block_texts.append(paragraph_text)
-                print(block_texts)
-        new_paragraphs.append(new_paragraph)
-
-    for block in blocks:
-        for paragraph in block.paragraphs:
-            if paragraph not in merged_paragraphs:
-                unmerged_paragraphs.append(paragraph)
-
-    return new_paragraphs + unmerged_paragraphs
-
-
-def merge_1d(paragraphs, distance_threshold=0.05, sort_x = False, delimeter=" | "):
-
-    points = []
-    for paragraph in paragraphs:
-        ys = []
-        for word in paragraph.words:
-            for v in word.bounding_box.normalized_vertices:
-                ys.append(v.y)
-        points.append((0, np.mean(ys)))
-
-    labels = AgglomerativeClustering(n_clusters=None, distance_threshold=distance_threshold).fit_predict(points)
-
-    blocks = defaultdict(list)
-    for paragraph, cluster_id in zip(paragraphs, labels):
-        blocks[cluster_id].append(paragraph)
-
-    lines = []
-    for cluster_id, paragraphs in blocks.items():
-        ys = []
-        texts = []
-        for paragraph in paragraphs:
-            xs = []
-            text = ""
-            for word in paragraph.words:
-                for v in word.bounding_box.normalized_vertices:
-                    xs.append(v.x)
-                    ys.append(v.y)
-                text += get_text(word)
-            texts.append([np.min(xs), text.strip().replace("\n", " ")])
-        if sort_x:
-            texts = sorted(texts, key=lambda x: x[0])
-        texts = [text for _, text in texts]
-        lines.append([np.mean(ys), delimeter.join(texts)])
-
-    lines = sorted(lines, key=lambda x: x[0])
-    texts = [line for _, line in lines]
-    return texts
-
 
 def merge_lines(blocks, scale_threshold=2, delimeter=" | "):
 
